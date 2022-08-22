a90a782f7e04f257658e811e15db3f1f46df3eda
keagan erasmus <keaganerasmus12@gmail.com>
2022-08-22 13:17:54 +1200

    created the board

---
+++ ChessMaze/ChessBoardModel/Board.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        public int Size { get; set; }
+
+        public Cell[,] theGrid { get; set; }
+
+        public Board (int s)
+        {
+            // Initial board size
+            Size = s;
+
+            theGrid = new Cell[Size, Size];
+
+            // filling 2D array with cells
+            for (int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves(Cell currentCell, Part chessPiece)
+        {
+            // clear the board
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+            // find all the legal moves
+
+            switch (chessPiece)
+            {
+                case (Part)'n':
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case (Part)'b':
+                    break;
+
+                case (Part)'q':
+                    break;
+
+                case (Part)'k':
+                    break;
+
+                case (Part)'r':
+                    break;
+            }
+        }
+    }
+}
--- ChessMaze/ChessMaze/Program.cs
+++ ChessMaze/ChessMaze/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace ChessMaze
+﻿using ChessBoardModel;
+
+namespace ChessMaze
 {
     class Program
     {
+        static Board myBoard = new Board(8);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // show empty board
+            printBoard(myBoard);
+
+
+            Console.ReadLine();
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            // display chess board: 'X' = current piece, '+' = legal next move, E = empty
+            for (int x = 0; x < myBoard.Size; x++)
+            {
+                for (int y = 0; y < myBoard.Size; y++)
+                {
+                    Cell c = myBoard.theGrid[x, y];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write('X');
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write('+');
+                    }
+                    else
+                    {
+                        Console.Write("e");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("====================");
         }
     }
 }
