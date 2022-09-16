c9be0c837e8045fcd3557ff7bfcddb59fc630765
keagan erasmus <keaganerasmus12@gmail.com>
2022-09-16 14:08:53 +1200

    .

--- ChessMaze/ChessMazeTests/TestBoardSize.cs
+++ ChessMaze/ChessMazeTests/TestBoardSize.cs
@@ -4,13 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ChessMazeTests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestBoardSize
     {
         [TestMethod]
-        public void TestBoardSize()
+        public void TestBoardSizeCorrect()
         {
             int expected = 8;
             Board myBoard = new(8);
+
+            int actual = myBoard.Size;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestBoardSizeFail()
+        {
+            int expected = 8;
+            Board myBoard = new(4);
 
             int actual = myBoard.Size;
 
